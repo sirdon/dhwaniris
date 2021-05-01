d873d394200177f5958a31af6c7773c467327062
sirdon <inform2theamit@gmail.com>
2021-05-01 23:53:21 +0530

    dhwani-ass complete

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.use("/api/user", UserRoutes);
 app.use("/api", authMiddleware, StateDisRoutes);
 app.use("/api", authMiddleware, ChildRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
