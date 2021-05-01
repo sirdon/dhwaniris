fffe07e7dbe941600ddebfc6b0e0bda721ab1e81
sirdon <inform2theamit@gmail.com>
2021-05-01 14:55:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
